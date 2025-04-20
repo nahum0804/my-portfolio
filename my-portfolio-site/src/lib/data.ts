5fa289029df3b15c88ee1d215364efbc58e1bece
Nahum Murillo <n4hummuro227@gmail.com>
2025-04-19 22:40:03 -0600

    My Portfolio - Styles, Responsive and Structure defined!

---
+++ my-portfolio-site/src/lib/data.ts
@@ -0,0 +1,88 @@
+export const personalInfo = {
+    name: "Nahúm Murillo",
+    location: "San Carlos, Costa Rica",
+    email: "n4hummuro227@gmail.com",
+    github: "https://github.com/nahum0804",
+    linkedin: "www.linkedin.com/in/nahum-murillo",
+  };
+  
+  export const workExperience = [
+    {
+      company: "Tecnológico de Costa Rica",
+      location: "San Carlos, Costa Rica (Hybrid)",
+      position: "Research Assistant - Developer",
+      period: "Feb 2025 - Present",
+      achievements: [
+        "Text Here!.",
+      ],
+    },
+    {
+      company: "Tecnológico de Costa Rica",
+      location: "San Carlos, Costa Rica (Hybrid)",
+      position: "Research Assistant - Developer",
+      period: "Feb 2024 - Dec 2024",
+      achievements: [
+        "Text Here",
+      ],
+    }
+  ];
+  
+  export const education = [
+    {
+      institution: "Tecnológico de Costa Rica",
+      location: "San Carloss, Costa Rica",
+      degree: "Bachelor - Software Engineer",
+      period: "Feb 2023 -  Feb 2027",
+      achievements: [
+        "Something here",
+      ],
+    },
+  ];
+  export const skills = {
+    programmingLanguages: [
+      "TypeScript",
+      "JavaScript",
+      "Python",
+      "Java"
+    ],
+    machineLearning: [
+      "RNN",
+      "CNN",
+      "Open CV",
+      "Pandas",
+      "Numpy", 
+      "Tensorflow",
+      "Keras",
+      "Pytorch", 
+    ],
+    frontendDevelopment: [
+      "Nextjs",
+      "Reactjs",
+      "Tailwind CSS",
+      "HTML",
+      "CSS",
+      "Pug",
+      "Vuejs"
+    ],
+    backendDevelopment: ["Nodejs", "Expressjs", "Flask", "Spring Boot"],
+    databaseAndStorage: ["PostgreSQL", "MySQL Server", "Mongo DB"],
+    cloudAndDevOps: [],
+    toolsAndServices: [],
+  };
+  
+  export const projects = [
+    {
+      title: "Project 1",
+      github: "#",
+      description: [
+        "Description",
+      ],
+    },
+    {
+      title: "Project 2",
+      github: "#",
+      description: [
+        "Description",
+      ],
+    },
+  ];
